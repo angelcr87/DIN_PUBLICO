2f4d9eedc8fe390ca122a014c80dfd9b85de7ba2
Angel <angel.casanova.87@gmail.com>
2020-02-28 19:06:29 -0600

    publicacion 1

---
+++ Scripts/misValidacionesScript.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    //VALIDACIONES VISTAS DE DIRECTORIO
+
+    //para el formato de la captura
+    $("#cel_gerente").mask("(000)0-00-0000");
+    $("#cel_encargado1").mask("(000)0-00-0000");
+    $("#cel_encargado2").mask("(000)0-00-0000");
+
+    $("#telefono_suc").mask("(000)0-00-0000");
+
+    $("#ext_suc").mask("0000");
+
+    //END VALIDACIONES VISTAS DE DIRECTORIO
+
+
+});
