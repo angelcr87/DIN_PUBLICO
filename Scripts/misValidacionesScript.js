3436d2dd919b3decb7c0ae6df71163a0bec0bfe2
Angel <angel.casanova.87@gmail.com>
2020-04-12 20:21:14 -0500

    publicacion 15

--- Scripts/misValidacionesScript.js
+++ Scripts/misValidacionesScript.js
@@ -6,13 +6,16 @@
     $("#cel_gerente").mask("(000)0-00-0000");
     $("#cel_encargado1").mask("(000)0-00-0000");
     $("#cel_encargado2").mask("(000)0-00-0000");
+    $("#cel_encargado3").mask("(000)0-00-0000");
+    $("#cel_encargado4").mask("(000)0-00-0000");
+    $("#cel_encargado5").mask("(000)0-00-0000");
 
     $("#telefono_suc").mask("(000)0-00-0000");    
     $("#telefonoContacto").mask("(000)0-00-0000");
 
     $("#ext_suc").mask("0000");
 
-    correoContacto
+
 
   
 
